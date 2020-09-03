1546b37e8d13a0381cf48b7e72b0a42c658f5ce3
Illia <bukovskii@ua.fm>
2020-09-03 22:15:02 +0300

    secondary commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
+//подключаем модули
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+//деоаем маодуль доступным для других модулей
 module.exports = {
+    // точка входа
     entry: {
         app: './src/index.js'
     },
+    //точка куда файл будет выходить,
+    // вебпак собирает бандл и помещает его в filename,
+    //после запуска (npm run build ) запускается вебпак который создаст оптимизированый бандл и поместит его в dist
     output: {
         filename: '[name].js',
+        //путь
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist'
     },
     module: {
         rules: [{
+            // тип обрабатываемого файла(1 часть лоадера)
         test:/\.js$/,
+        //лоадеры используются для преоброзования перед генерацией,
+        //подключаются в масив rules 
+        //нужно устанавливать для работы с стилями,картинками и т.д.
         loader: 'babel-loader',
         exclude: '/node_modules/'
         },
         {
+            //тип обрабатываемого файла
             test:/\.scss$/,
             use: [
+                // лоадер используемый для обр. данного типа файлов
                  'style-loader',
                 MiniCssExtractPlugin.loader,
                 {
@@ -35,6 +48,7 @@ module.exports = {
             ]
         },
         {
+            // npm i css-loader -D
             test:/\.css$/,
             use: [
                 'style-loader',
